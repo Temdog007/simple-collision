ed0c915f97f2ffa63bbe05f2a7d98df161b395f6
Temitope Alaga <temdog007@yahoo.com>
2020-10-29 01:00:39 -0700

    Fix

--- src/collision3d/shapes/aabb.rs
+++ src/collision3d/shapes/aabb.rs
@@ -40,10 +40,10 @@ impl<'a, N: PhysicsScalar> FromIterator<&'a Vector3<N>> for AxisAlignedBoundingB
         let mut end = Vector3::from_element(Bounded::min_value());
         for v in iter {
             for (s, point) in start.iter_mut().zip(v.iter()) {
-                *s = num_traits::clamp_min(*s, *point);
+                *s = num_traits::clamp_min(*point, *s);
             }
             for (s, point) in end.iter_mut().zip(v.iter()) {
-                *s = num_traits::clamp_max(*s, *point);
+                *s = num_traits::clamp_max(*point, *s);
             }
         }
         AxisAlignedBoundingBox { start, end }
